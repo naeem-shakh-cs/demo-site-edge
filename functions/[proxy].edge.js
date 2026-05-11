f7c8caecb5a705d1d7932ff556fe4b626b071c24
Naeem Shaikh <naeem.shaikh@contentstack.com>
2026-05-11 22:48:24 +0530

    break streaming at edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,3 +1,11 @@
 export default async function handler(req, context) {
-  return fetch(req)
+  const originResponse = await fetch(req);
+
+  // Consuming the body here forces the edge to wait for the full response
+  const body = await originResponse.text();
+
+  return new Response(body, {
+    status: originResponse.status,
+    headers: originResponse.headers,
+  });
 }
